4db129b22774cd2564661b0127554741308e3dca
KirillGluhov <122099055+KirillGluhov@users.noreply.github.com>
2023-06-04 21:50:39 +0700

    При нажатии на Esc - возврат в меню

--- Assets/CreationWithShooms.cs
+++ Assets/CreationWithShooms.cs
@@ -2,12 +2,23 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CreationWithShooms : MonoBehaviour
 {
     public GameObject blockPrefabFirst;
     public GameObject blockPrefabSecond;
+    public int heightOfCentrePlits;
+    public string nextScene;
+
+    public GameObject Muskrat;
+    public GameObject Colobus;
+    public GameObject Gecko;
+    public GameObject Purdu;
+    public GameObject Sparrow;
+    public GameObject Taipan;
     public class DiamondSquareAlgorithm
     {
         private int size;
@@ -105,8 +116,8 @@ public class CreationWithShooms : MonoBehaviour
     void Start()
     {
         int size = 17;
-        System.Random random = new System.Random();
-        float roughness = (float)random.NextDouble();
+        System.Random newRandom = new System.Random();
+        float roughness = (float)newRandom.NextDouble();
 
         DiamondSquareAlgorithm diamondSquare = new DiamondSquareAlgorithm(size);
         float[,] heightMap = diamondSquare.GenerateHeightMap(roughness);
@@ -117,9 +128,7 @@ public class CreationWithShooms : MonoBehaviour
             {
                 heightMap[x, y] += 0.4F;
                 heightMap[x, y] *= 50;
-                Console.WriteLine(heightMap[x, y] + " ");
             }
-            Console.WriteLine();
 
         }
 
@@ -140,7 +149,37 @@ public class CreationWithShooms : MonoBehaviour
 
                 if (i == 0 & j == 0)
                 {
+                    heightOfCentrePlits = h;
                     GameObject.Find("Goo").transform.position = new Vector3(0, (h+1) * 0.0625F, 0);
+                }
+                else if ((float)newRandom.NextDouble() > 0.9)
+                {
+                    float chooseAnimal = UnityEngine.Random.Range(0, 6);
+
+                    if (chooseAnimal < 1)
+                    {
+                        GameObject newMuskrat = Instantiate(Muskrat, new Vector3(j, h * 0.0625F, i), Quaternion.identity);
+                    }
+                    else if (chooseAnimal < 2)
+                    {
+                        GameObject newColobus = Instantiate(Colobus, new Vector3(j, h * 0.0625F, i), Quaternion.identity);
+                    }
+                    else if (chooseAnimal < 3)
+                    {
+                        GameObject newGecko = Instantiate(Gecko, new Vector3(j, h * 0.0625F, i), Quaternion.identity);
+                    }
+                    else if (chooseAnimal < 4)
+                    {
+                        GameObject newPurdu = Instantiate(Purdu, new Vector3(j, h * 0.0625F, i), Quaternion.identity);
+                    }
+                    else if (chooseAnimal < 5)
+                    {
+                        GameObject newSparrow = Instantiate(Sparrow, new Vector3(j, h * 0.0625F, i), Quaternion.identity);
+                    }
+                    else if (chooseAnimal < 6)
+                    {
+                        GameObject newTaipan = Instantiate(Taipan, new Vector3(j, h * 0.0625F, i), Quaternion.identity);
+                    }
                 }
 
             }
@@ -150,6 +189,15 @@ public class CreationWithShooms : MonoBehaviour
 
     void Update()
     {
-        
+        if (GameObject.Find("Goo").transform.position.y < -10)
+        {
+            GameObject.Find("Goo").transform.rotation = Quaternion.LookRotation(new Vector3(0,0,0) );
+            GameObject.Find("Goo").transform.position = new Vector3(0, (heightOfCentrePlits + 2) * 0.0625F, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(nextScene);
+        }
     }
 }
