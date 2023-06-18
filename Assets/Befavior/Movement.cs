8137ea01a24af592cb40139fa97ed6e763e4dc46
KirillGluhov <122099055+KirillGluhov@users.noreply.github.com>
2023-06-18 20:32:59 +0700

    Процедурная генерация
    Генерация/Регенерация/Отключение  чанков. Только вода теперь - блок на поверхности, а не Quad

---
+++ Assets/Befavior/Movement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 0.5f;
+    public float jumpForce = 5f;
+    private bool isJumping = false;
+    private Rigidbody playerRigidbody;
+    private Camera mainCamera;
+    private CharacterController controller;
+
+    private void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+        mainCamera = Camera.main;
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 cameraForward = mainCamera.transform.forward;
+        cameraForward.y = 0;
+        cameraForward = cameraForward.normalized;
+
+
+        Vector3 movement = (cameraForward * moveVertical + mainCamera.transform.right * moveHorizontal) * speed;
+
+        playerRigidbody.AddForce(movement);
+
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isJumping = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isJumping = false;
+    }
+}
---
+++ Assets/Generation/GameWorld.cs
@@ -0,0 +1,133 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameWorld : MonoBehaviour
+{
+    private const int ViewRadius = 5;
+
+    public Dictionary<Vector2Int, ChunkData> ChunkDatas = new Dictionary<Vector2Int, ChunkData>();
+    public ChunkGenerator Chunkprefab;
+    public TerrainGenerator Generator;
+
+    public GameObject player;
+
+    private Camera mainCamera;
+    private Vector2Int currentPlayerChunk;
+    void Start()
+    {
+        mainCamera = Camera.main;
+
+        Generator.Init();
+        StartCoroutine(Generate(false));
+    }
+
+    private IEnumerator Generate(bool wait)
+    {
+
+        for (int x = currentPlayerChunk.x - ViewRadius; x < currentPlayerChunk.x + ViewRadius; x++)
+        {
+            for (int y = currentPlayerChunk.y - ViewRadius; y < currentPlayerChunk.y + ViewRadius; y++)
+            {
+                Vector2Int chunkPosition = new Vector2Int(x,y);
+
+                if (ChunkDatas.ContainsKey(new Vector2Int(x, y))) 
+                {
+                    var value = ChunkDatas[new Vector2Int(x, y)];
+
+                    for (int i = 0; i < value.infoAboutChunk.AnimalsAndTrees.Count; i++)
+                    {
+                        value.infoAboutChunk.AnimalsAndTrees[i].SetActive(true);
+                    }
+                    value.Renderer.gameObject.SetActive(true);
+                    continue; 
+                }
+
+                LoadChunk(chunkPosition);
+
+                if (wait) yield return new WaitForSecondsRealtime(0.2f);
+                
+            }
+        }
+    }
+
+    public void LoadChunk(Vector2Int chunkPosition)
+    {
+        float xPos = chunkPosition.x * ChunkGenerator.ChunkWidth * ChunkGenerator.BlockScale;
+        float zPos = chunkPosition.y * ChunkGenerator.ChunkWidth * ChunkGenerator.BlockScale;
+
+        ChunkData chunkData = new ChunkData();
+
+        chunkData.Chunkposition = chunkPosition;
+        chunkData.infoAboutChunk = Generator.GenerateTerrain(xPos, zPos);
+        ChunkDatas.Add(chunkPosition, chunkData);
+
+        var chunk = Instantiate(Chunkprefab, new Vector3(xPos, 0, zPos), Quaternion.identity, transform);
+        chunk.ChunkData = chunkData;
+        chunk.ParentWorld = this;
+
+        chunkData.Renderer = chunk;
+    }
+
+    private void Update()
+    {
+        Vector3Int playerWorldPos = Vector3Int.FloorToInt(player.transform.position / ChunkGenerator.BlockScale);
+        Vector2Int playerChunk = GetChunkContainingBlock(playerWorldPos);
+
+        if (playerChunk != currentPlayerChunk)
+        {
+            currentPlayerChunk = playerChunk;
+            StartCoroutine(Generate(true));
+        }
+
+        List<Vector2Int> chunksToRemove = new List<Vector2Int>();
+        foreach (var chunkPos in ChunkDatas.Keys)
+        {
+            float distance = Vector2Int.Distance(chunkPos, playerChunk);
+            if (distance > 2*ViewRadius)
+            {
+                chunksToRemove.Add(chunkPos);
+            }
+        }
+
+        foreach (var chunkPos in chunksToRemove)
+        {
+            UnloadChunk(chunkPos);
+        }
+    }
+
+    public Vector2Int GetChunkContainingBlock(Vector3Int blockWorldPos)
+    {
+        Vector2Int chunkPosition = new Vector2Int(blockWorldPos.x/ChunkGenerator.ChunkWidth, blockWorldPos.z/ChunkGenerator.ChunkWidth);
+
+        if (blockWorldPos.x < 0)
+        {
+            chunkPosition.x--;
+        }
+
+        if (blockWorldPos.y < 0)
+        {
+            chunkPosition.y--;
+        }
+
+        return chunkPosition;
+    }
+
+    private void UnloadChunk(Vector2Int chunkPosition)
+    {
+        if (ChunkDatas.ContainsKey(chunkPosition))
+        {
+            ChunkData chunkData = ChunkDatas[chunkPosition];
+
+            if (chunkData.Renderer != null)
+            {
+                for (int i = 0; i < chunkData.infoAboutChunk.AnimalsAndTrees.Count; i++)
+                {
+                    chunkData.infoAboutChunk.AnimalsAndTrees[i].SetActive(false);
+                }
+
+                chunkData.Renderer.gameObject.SetActive(false);
+            }
+        }
+    }
+}
