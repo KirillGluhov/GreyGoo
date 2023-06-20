124c34e6daa2011f9a0ae12a3b1837fc835338ab
Yanyshevskaya Karina <karinaya94@gmail.com>
2023-06-20 18:51:38 +0700

    changes extras and name

--- Assets/SlimUI/Modern Menu 1/Scripts/Managers/UIMenuManager.cs
+++ Assets/SlimUI/Modern Menu 1/Scripts/Managers/UIMenuManager.cs
@@ -9,7 +9,7 @@ namespace SlimUI.ModernMenu{
 		private Animator CameraObject;
 
 		// campaign button sub menu
-        [Header("MENUS")]
+        [Header("GreyGoo")]
         [Tooltip("The Menu for when the MAIN menu buttons")]
         public GameObject mainMenu;
         [Tooltip("THe first list of buttons")]
